472fee2d3f0f8d000325e0461e6db9cd86395b1e
lyuhw1023 <lyuhw11023@gmail.com>
2022-12-06 00:32:39 +0900

    액션바를 툴바로 수정

--- app/src/main/java/kr/ac/hallym/smartportfolio/MainActivity.kt
+++ app/src/main/java/kr/ac/hallym/smartportfolio/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
         toggle.syncState()
 
