d2b94799bd4151e48dddc2bb1d73835b242fcbbf
lyuhw1023 <lyuhw11023@gmail.com>
2022-12-10 00:05:24 +0900

    LyuHyeWon 화면 디자인 수정

--- app/src/main/java/kr/ac/hallym/smartportfolio/LyuHyeWon.kt
+++ app/src/main/java/kr/ac/hallym/smartportfolio/LyuHyeWon.kt
@@ -10,23 +10,29 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+//import androidx.navigation.Navigation
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import kr.ac.hallym.smartportfolio.databinding.ActivityLyuhyewonBinding
 
-class Lyuhyewon : AppCompatActivity() {
-    lateinit var binding: ActivityLyuhyewonBinding
+
+class LyuHyeWon : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_lyuhyewon)
-        binding = ActivityLyuhyewonBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_lyuHyeWon)
+        val binding = ActivityLyuhyewonBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawer,
+            R.string.drawer_opened,
+            R.string.drawer_closed
+        )
         toggle.syncState()
 
         /*binding.mainDrawerView.setNavigationItemSelectedListener {
@@ -44,9 +50,11 @@ class Lyuhyewon : AppCompatActivity() {
 
         viewPager.adapter = MyFragmentPagerAdapter(this)
 
+
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = tabTitleArray[position]
         }.attach()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -54,7 +62,7 @@ class Lyuhyewon : AppCompatActivity() {
 
         val menuItem = menu?.findItem(R.id.menu_search)
         val searchView = menuItem?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 Log.d("lyu", "$query will be searched")
                 return true
@@ -70,17 +78,15 @@ class Lyuhyewon : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
-
-
 class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(activity) {
     val fragments: List<Fragment>
-
     init {
         fragments = listOf(IntroFragment(), ScoreFragment(), SchoolFragment())
     }
@@ -88,4 +94,6 @@ class MyFragmentPagerAdapter(activity: FragmentActivity): FragmentStateAdapter(a
     override fun getItemCount(): Int = fragments.size
 
     override fun createFragment(position: Int): Fragment = fragments[position]
+
 }
+
