be853b267723bc4b7020f95c6f74ed304e5d4257
lyuhw1023 <lyuhw11023@gmail.com>
2022-12-11 21:31:34 +0900

    projectTest 화면 - 디테일 화면 추가

---
+++ app/src/main/java/kr/ac/hallym/smartportfolio/ProjectOne.kt
@@ -0,0 +1,49 @@
+package kr.ac.hallym.smartportfolio
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kr.ac.hallym.smartportfolio.databinding.ActivityProjectOneBinding
+import kr.ac.hallym.smartportfolio.databinding.ActivityProjectTestBinding
+
+class ProjectOne : AppCompatActivity() {
+    lateinit var binding: ActivityProjectOneBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_project_one)
+        binding = ActivityProjectOneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.GitHub1.setOnClickListener{
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/lyuhw1023/smart_portfolio"))
+            startActivity(intent)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.menu_logout -> {
+                val intent = Intent(this, login::class.java)
+                startActivity(intent)
+                finishAffinity()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
